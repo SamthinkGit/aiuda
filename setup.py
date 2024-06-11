80867032758118e0d0d3cc80c83c5963f9639fcd
SamthinkGit <sebastianmayorquin@gmail.com>
2024-06-11 12:52:42 +0200

    version 0.0.2 released

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='aiuda',
-    version='0.0.1',
+    version='0.0.2',
     packages=find_packages(),
     install_requires=requirements,
     author='Sebastian Mayorquín',
